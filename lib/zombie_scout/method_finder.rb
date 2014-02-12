73960d98c016c05079505ce6e95ff2a5609c9ea7
Dan Bernier <danbernier@gmail.com>
2014-02-11 21:34:50 -0500

    When parsing RubySource, exclude private method calls, because we KNOW they're called

--- lib/zombie_scout/method_finder.rb
+++ lib/zombie_scout/method_finder.rb
@@ -9,12 +9,15 @@ module ZombieScout
     end
 
     def find_methods
-      @method_names = []
+      @methods = []
+      @private_method_calls = []
 
       node = Parser::CurrentRuby.parse(@ruby_source.source)
       process(node)
 
-      @method_names 
+      @methods.reject { |method|
+        @private_method_calls.include?(method.name)
+      }
     end
 
     def on_def(node) 
@@ -29,12 +32,20 @@ module ZombieScout
       process(body)
     end
 
+    def on_send(node)
+      receiver, method_name, args = *node
+      if receiver.nil?  # Then it's a private method call
+        @private_method_calls << method_name
+      end
+      process(args)
+    end
+
     private 
 
     def stash_method(method_name, node_location)
       line_number = node_location.line
       location = [@ruby_source.path, line_number].join(":")
-      @method_names << Method.new(method_name, location)
+      @methods << Method.new(method_name, location)
     end
   end
 end
--- spec/zombie_scout/method_finder_spec.rb
+++ spec/zombie_scout/method_finder_spec.rb
@@ -25,4 +25,26 @@ describe ZombieScout::MethodFinder, '#find_methods' do
     expect(methods[1].name).to eq :fizz
     expect(methods[1].location).to eq 'lib/fizzbuzz.rb:3'
   end 
+
+  it 'excludes private method calls, since we KNOW they are called' do
+    ruby_code = "
+      class FizzBuzz
+        def fizz
+          magick_helper
+        end
+
+        private
+        def magick_helper
+          'magick sauce'
+        end
+      end
+    "
+    ruby_source = double(:ruby_source, path: 'lib/fizzbuzz.rb', source: ruby_code)
+
+    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods
+
+    expect(methods.size). to eq 1
+    expect(methods[0].name).to eq :fizz
+    expect(methods.map(&:name)).not_to include :magick_helper
+  end
 end
