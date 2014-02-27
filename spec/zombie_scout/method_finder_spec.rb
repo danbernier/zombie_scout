e81fe174fa9bb2de7d2e14bb01f8f542f9248f9b
Dan Bernier <danbernier@gmail.com>
2014-02-26 20:55:12 -0500

    Skinny down the method_finder specs

--- spec/zombie_scout/method_finder_spec.rb
+++ spec/zombie_scout/method_finder_spec.rb
@@ -1,114 +1,110 @@
 require 'spec_helper'
-
 require 'zombie_scout/method_finder'
 
 describe ZombieScout::MethodFinder, '#find_methods' do
-  it 'can find methods in ruby source' do
-    ruby_code = "
-    class FizzBuzz
-      def fizz
-        'plop plop'
-      end
+  let(:ruby_source) {
+    double(:ruby_source, path: 'lib/fizzbuzz.rb', source: ruby_code)
+  }
+  let(:zombies) {
+    ZombieScout::MethodFinder.new(ruby_source).find_methods.sort_by(&:name)
+  }
 
-      def self.buzz
-        'bzz bzz bzz'
-      end
+  context 'when a ruby file has instance or class methods' do
+    let(:ruby_code) {
+      "class FizzBuzz
+         def fizz
+           'plop plop'
+         end
+
+         def self.buzz
+           'bzz bzz bzz'
+         end
+       end"
+    }
+
+    it 'can find the methods' do
+      expect(zombies[0].name).to eq :buzz
+      expect(zombies[0].location).to eq 'lib/fizzbuzz.rb:6'
+
+      expect(zombies[1].name).to eq :fizz
+      expect(zombies[1].location).to eq 'lib/fizzbuzz.rb:2'
     end
-    "
-    ruby_source = double(:ruby_source, path: 'lib/fizzbuzz.rb', source: ruby_code)
-
-    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods.sort_by(&:name)
-
-    expect(methods[0].name).to eq :buzz
-    expect(methods[0].location).to eq 'lib/fizzbuzz.rb:7'
-
-    expect(methods[1].name).to eq :fizz
-    expect(methods[1].location).to eq 'lib/fizzbuzz.rb:3'
   end
 
-  it 'can find attr_readers' do
-    ruby_code = "
-    class Book
-      attr_reader :title, :author
+  context 'when a ruby file has attr_readers' do
+    let(:ruby_code) { 
+      "class Book
+         attr_reader :title, :author
+       end"
+    }
+
+    it 'can find attr_readers' do
+      expect(zombies.map(&:name)).to eq(%i[author title])
     end
-    "
-    ruby_source = double(:ruby_source, path: 'lib/book.rb', source: ruby_code)
-
-    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods
-    expect(methods.map(&:name).sort).to eq(%i[author title])
   end
 
-  it 'can find attr_writers' do
-    ruby_code = "
-    class Book
-      attr_writer :title, :author
+  context 'when a ruby file has attr_writers' do
+    let(:ruby_code) { 
+      "class Book
+         attr_writer :title, :author
+       end"
+    }
+    it 'can find attr_writers' do
+      expect(zombies.map(&:name)).to eq(%i[author= title=])
     end
-    "
-    ruby_source = double(:ruby_source, path: 'lib/book.rb', source: ruby_code)
-
-    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods
-    expect(methods.map(&:name).sort).to eq(%i[author= title=])
   end
 
-  it 'can find attr_writers' do
-    ruby_code = "
-    class Book
-      attr_accessor :title, :author
+  context 'when a ruby file has attr_accessors' do
+    let(:ruby_code) {
+      "class Book
+         attr_accessor :title, :author
+       end"
+    }
+    it 'can find attr_accessors' do
+      expect(zombies.map(&:name)).to eq(%i[author author= title title=])
     end
-    "
-    ruby_source = double(:ruby_source, path: 'lib/book.rb', source: ruby_code)
-
-    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods
-    expect(methods.map(&:name).sort).to eq(%i[author author= title title=])
   end
 
-  it 'can find Forwardable delegators defined with def_delegator' do
-    ruby_code = "
-      class RecordCollection
-        extend Forwardable
-        def_delegator :@records, :[], :record_number
-      end
-    "
-    ruby_source = double(:ruby_source, path: 'lib/record_collection.rb', source: ruby_code)
-
-    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods
-    expect(methods.size).to eq(1)
-    expect(methods[0].name).to eq(:record_number)
+  context 'when a ruby file uses Forwardable::def_delegator' do
+    let(:ruby_code) { 
+      "class RecordCollection
+         extend Forwardable
+         def_delegator :@records, :[], :record_number
+       end"
+    }
+    it 'can find methods created by def_delegator' do
+      expect(zombies.map(&:name)).to match_array([:record_number])
+    end
   end
 
-  it 'can find Forwardable delegators defined with def_delegators' do
-    ruby_code = "
-      class RecordCollection
-        extend Forwardable
-        def_delegators :@records, :size, :<<, :map
-      end
-    "
-    ruby_source = double(:ruby_source, path: 'lib/record_collection.rb', source: ruby_code)
-
-    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods
-    expect(methods.size).to eq(3)
-    expect(methods.map(&:name).sort).to eq(%i[<< map size])
+  context 'when a ruby file uses Forwardable::def_delegators' do
+    let(:ruby_code) {
+      "class RecordCollection
+         extend Forwardable
+         def_delegators :@records, :size, :<<, :map
+       end"
+    }
+    it 'can find methods created by def_delegators' do
+      expect(zombies.map(&:name)).to eq(%i[<< map size])
+    end
   end
 
-  it 'excludes private method calls, since we KNOW they are called' do
-    ruby_code = "
-      class FizzBuzz
-        def fizz
-          magick_helper
-        end
+  context 'when a ruby file has private methods' do
+    let(:ruby_code) {
+      "class FizzBuzz
+         def fizz
+           magick_helper
+         end
 
-        private
-        def magick_helper
-          'magick sauce'
-        end
-      end
-    "
-    ruby_source = double(:ruby_source, path: 'lib/fizzbuzz.rb', source: ruby_code)
-
-    methods = ZombieScout::MethodFinder.new(ruby_source).find_methods
-
-    expect(methods.size).to eq 1
-    expect(methods[0].name).to eq :fizz
-    expect(methods.map(&:name)).not_to include :magick_helper
+         private
+         def magick_helper
+           'magick sauce'
+         end
+      end"
+    }
+    it 'excludes private method calls, since we KNOW they are called' do
+      expect(zombies.map(&:name)).to match_array([:fizz])
+      expect(zombies.map(&:name)).not_to include :magick_helper
+    end
   end
 end
