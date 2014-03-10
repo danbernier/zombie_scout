1701ec929310d815844169480c2a6525644566b6
Dan Bernier <dbernier@continuity.net>
2014-03-10 10:51:21 -0400

    Fix indenting in the specs

--- spec/zombie_scout/parser_spec.rb
+++ spec/zombie_scout/parser_spec.rb
@@ -32,14 +32,14 @@ describe ZombieScout::Parser do
     context 'when a ruby file has instance or class methods' do
       let(:ruby_code) {
         "class FizzBuzz
-         def fizz
-           'plop plop'
-         end
+           def fizz
+             'plop plop'
+           end
 
-         def self.buzz
-           'bzz bzz bzz'
-         end
-       end"
+           def self.buzz
+             'bzz bzz bzz'
+           end
+         end"
       }
 
       it 'can find the methods' do
@@ -55,8 +55,8 @@ describe ZombieScout::Parser do
       context "when they're declared with symbols, the normal way" do
         let(:ruby_code) {
           "class Book
-           attr_reader :title, :author
-         end"
+             attr_reader :title, :author
+           end"
         }
         it 'can find attr_readers' do
           expect(defined_methods.map(&:name)).to match_array(%i[author title])
@@ -65,9 +65,9 @@ describe ZombieScout::Parser do
       context "when they're declare with a splat from an array of symbols" do
         let(:ruby_code) {
           "class Book
-           attributes = %i(title author)
-           attr_reader *attributes
-         end"
+             attributes = %i(title author)
+             attr_reader *attributes
+           end"
         }
         it 'will ignore them' do
           expect(defined_methods).to be_empty
@@ -79,8 +79,8 @@ describe ZombieScout::Parser do
       context "when they're declared with symbols, the normal way" do
         let(:ruby_code) {
           "class Book
-           attr_writer :title, :author
-         end"
+             attr_writer :title, :author
+           end"
         }
         it 'can find attr_writers' do
           expect(defined_methods.map(&:name)).to match_array(%i[author= title=])
@@ -89,9 +89,9 @@ describe ZombieScout::Parser do
       context "when they're declare with a splat from an array of symbols" do
         let(:ruby_code) {
           "class Book
-           attributes = %i(title author)
-           attr_reader *attributes
-         end"
+             attributes = %i(title author)
+             attr_reader *attributes
+           end"
         }
         it 'will ignore them' do
           expect(defined_methods).to be_empty
@@ -103,8 +103,8 @@ describe ZombieScout::Parser do
       context "when they're declared with symbols, the normal way" do
         let(:ruby_code) {
           "class Book
-           attr_accessor :title, :author
-         end"
+             attr_accessor :title, :author
+           end"
         }
         it 'can find attr_accessors' do
           expect(defined_methods.map(&:name)).to match_array(%i[author author= title title=])
@@ -113,9 +113,9 @@ describe ZombieScout::Parser do
       context "when they're declare with a splat from an array of symbols" do
         let(:ruby_code) {
           "class Book
-           attributes = %i(title author)
-           attr_accessor *attributes
-         end"
+             attributes = %i(title author)
+             attr_accessor *attributes
+           end"
         }
         it 'will ignore them' do
           expect(defined_methods).to be_empty
@@ -126,9 +126,9 @@ describe ZombieScout::Parser do
     context 'when a ruby file uses Forwardable::def_delegator' do
       let(:ruby_code) {
         "class RecordCollection
-         extend Forwardable
-         def_delegator :@records, :[], :record_number
-       end"
+           extend Forwardable
+           def_delegator :@records, :[], :record_number
+         end"
       }
       it 'can find methods created by def_delegator' do
         expect(defined_methods.map(&:name)).to match_array([:record_number])
@@ -138,9 +138,9 @@ describe ZombieScout::Parser do
     context 'when a ruby file uses Forwardable::def_delegators' do
       let(:ruby_code) {
         "class RecordCollection
-         extend Forwardable
-         def_delegators :@records, :size, :<<, :map
-       end"
+           extend Forwardable
+           def_delegators :@records, :size, :<<, :map
+         end"
       }
       it 'can find methods created by def_delegators' do
         expect(defined_methods.map(&:name)).to match_array(%i[<< map size])
@@ -150,9 +150,9 @@ describe ZombieScout::Parser do
     context 'when a rails model has scopes' do
       let(:ruby_code) {
         "class Post
-         scope :published, -> { where(published: true) }
-         scope :draft, -> { where(published: true) }
-       end"
+           scope :published, -> { where(published: true) }
+           scope :draft, -> { where(published: true) }
+         end"
       }
       it 'can find scopes' do
         expect(defined_methods.map(&:name)).to match_array(%i[draft published])
@@ -162,19 +162,19 @@ describe ZombieScout::Parser do
     context 'when a ruby file has private methods' do
       let(:ruby_code) {
         "class FizzBuzz
-         def fizz
-           magick_helper
-         end
+           def fizz
+             magick_helper
+           end
 
-         private
-         def magick_helper
-           'magick sauce'
-         end
+           private
+           def magick_helper
+             'magick sauce'
+           end
 
-         def other_helper
-           'boo'
-         end
-      end"
+           def other_helper
+             'boo'
+           end
+         end"
       }
       it 'excludes private method calls, since we KNOW they are called' do
         expect(defined_methods.map(&:name)).to match_array([:fizz, :magick_helper, :other_helper])
