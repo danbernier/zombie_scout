ba74ab2c8cf2f5e609ada2005183616f39a9fbb5
Dan Bernier <danbernier@gmail.com>
2014-03-22 12:32:00 -0400

    (wip) playing w/ thor

--- lib/zombie_scout/app.rb
+++ lib/zombie_scout/app.rb
@@ -12,5 +12,57 @@ module ZombieScout
 
       puts ZombieScout::Formatter.format(options[:format], mission, report)
     end
+
+    desc "playing", 'hacking on thor to see what it can do'
+    def playing(*thingy)
+      #say(thingy, :green) #:blue) #:red) # :yellow
+
+      require 'optparse'
+      require 'pp'
+      require 'ostruct'
+      require 'shellwords'
+   
+      pp ZombieScoutConfigParser.parse(project_configuration + ARGV)   
+    end
+
+    no_tasks do
+      def project_configuration
+        args_from_options_file('.zombie_scout') 
+      end
+
+      def args_from_options_file(path)
+        return [] unless File.exist?(path)
+        config_string = File.read(path)
+        config_string.split(/\n+/).flat_map(&:shellsplit)
+      end
+    end
+  end
+end
+
+module ZombieScoutConfigParser
+  def self.parse(args)
+    options = OpenStruct.new
+    options.grep_include = '*.rb'
+
+    opt_parser = OptionParser.new do |opts|
+
+      opts.on('--grep-exclude=FILE_PATTERN', 
+              'When grepping, skip files and directories matching FILE_PATTERN') do |pattern|
+        options.grep_exclude = pattern
+      end
+
+      opts.on('--grep-include=FILE_PATTERN',
+              'When grepping, search only files that match FILE_PATTERN') do |pattern|
+        options.grep_include = pattern
+      end
+
+      opts.on('--grep-exclude-dir=PATTERN',
+              'When grepping, directories that match PATTERN will be skipped.') do |pattern|
+        options.grep_exclude_dir = pattern
+      end
+    end
+
+    opt_parser.parse!(args)
+    options 
   end
 end
